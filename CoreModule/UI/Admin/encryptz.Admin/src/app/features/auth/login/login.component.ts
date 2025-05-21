499c466fe86c8fdac5770544c25ce37cfbef3772
ZeydMohammed <seydmohammed1999@gmail.com>
2025-05-21 21:24:11 +0530

    UI Changes

--- CoreModule/UI/Admin/encryptz.Admin/src/app/features/auth/login/login.component.ts
+++ CoreModule/UI/Admin/encryptz.Admin/src/app/features/auth/login/login.component.ts
@@ -3,6 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+
 
 
 @Component({
@@ -11,7 +17,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   imports: [
     FormsModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
@@ -20,7 +30,7 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   onLogin() {
     if (this.email === 'admin@example.com' && this.password === 'password') {
